b11bc3fe430b4d94dc13d97f025e90be4774d8ab
Juan Abrate <juan.abrate21@gmail.com>
2021-03-11 17:05:29 -0300

    token y credenciales

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState  , useEffect} from 'react';
+import axios from 'axios';
+import { Credentials } from './components/Credentials';
 
 function App() {
+
+  const [token, setToken] = useState('');  
+
+  const spotify = Credentials();
+  
+
+  useEffect(() => {
+
+    axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)      
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+    .then(tokenResponse => {      
+
+      setToken(tokenResponse.data.access_token);
+
+      console.log(token);      
+    });
+
+  }, [spotify.ClientId, spotify.ClientSecret]); 
+
+  console.log(token);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />        
+      </header> */}      
     </div>
   );
 }
---
+++ src/components/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: 'b3f4d64a249e41cda656ddc52f7de779',
+        ClientSecret: '5c48ec7d05b74c8e84c9cd6ab89659cd'
+    }
+}
+
+export { Credentials };
